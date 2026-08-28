a3a5aaabdda859739f57c9300661518cea7cb554
Coryb <y-crbnz25@outlook.com>
2026-08-28 11:52:13 -0600

    Se agregan configuraciones en la clase AtencionService y AuthService.

--- src/main/java/com/proyectocoryb/service/AtencionService.java
+++ src/main/java/com/proyectocoryb/service/AtencionService.java
@@ -1,4 +1,33 @@
 package com.proyectocoryb.service;
 
+import com.proyectocoryb.dto.request.AtencionRequestDTO;
+import com.proyectocoryb.dto.response.AtencionResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
 public interface AtencionService {
+
+    AtencionResponseDTO crearAtencion(AtencionRequestDTO requestDTO);
+
+    AtencionResponseDTO obtenerAtencionPorId(Long id);
+
+    Page<AtencionResponseDTO> listarTodas(Pageable pageable);
+
+    Page<AtencionResponseDTO> listarPorPaciente(Long pacienteId, Pageable pageable);
+
+    Page<AtencionResponseDTO> listarPorEmpleado(Long empleadoId, Pageable pageable);
+
+    Page<AtencionResponseDTO> listarPorEstado(Long empleadoId, Pageable pageable);
+
+    Page<AtencionResponseDTO> listarPorRangoFechas(LocalDateTime fechaInicio, LocalDateTime fechaFin, Pageable pageable);
+
+    Page<AtencionResponseDTO> buscarPorMotivo(String motivo, Pageable pageable);
+
+    AtencionResponseDTO actualizarAtencion(Long id, AtencionRequestDTO requestDTO);
+
+    void eliminarAtencion(Long id);
+
+    Page<AtencionResponseDTO> listarAtencionesPacienteAutenticado(Pageable pageable);
 }
--- src/main/java/com/proyectocoryb/service/AuthService.java
+++ src/main/java/com/proyectocoryb/service/AuthService.java
@@ -1,4 +1,13 @@
 package com.proyectocoryb.service;
 
+import com.proyectocoryb.dto.request.AuthLoginRequestDTO;
+import com.proyectocoryb.dto.request.AuthRegisterRequestDTO;
+import com.proyectocoryb.dto.response.AuthResponseDTO;
+import com.proyectocoryb.dto.response.MessageResponseDTO;
+
 public interface AuthService {
+
+    MessageResponseDTO registrar(AuthRegisterRequestDTO request);
+
+    AuthResponseDTO login(AuthLoginRequestDTO request);
 }
